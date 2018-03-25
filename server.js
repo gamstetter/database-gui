87be6524df79f3ec77d15f13178ca2a59abf3f21
Philip Tallo <tallopt@mail.uc.edu>
2018-03-25 15:13:17 -0400

    connection working

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
-var express = require('express');
-var app = express();
+var Connection = require('tedious').Connection;
+var config = {
+    userName: 'root',
+    password: '',
+    server: 'WIN-09DVBCGUN6H.Reporting_developer.windows.net'
+};
 
-var sql = require('mssql');
-
-app.get('/', function(req, res){
-    var config = {
-        user : 'root',
-        password : '',
-        server : 'localhost',
-        database : 'Reporting_developer'
-    };
-
-    sql.connect(config, function(err){
-        if (err){
-            console.log('ERROR!');
-
-        } else {
-            console.log('Connected');
-            var request = new sql.Request();
-
-            request.query('select * from work.statuses', function(err, recordset){
-                if (err){
-                    console.log('there was a problem querying!');
-                }
-
-                res.send(recordset);
-            })
-        }
-    });
-    
-    sql.close();
+var connection = new Connection(config);
+connection.on('connect', function(err) {
+    // If no error, then good to proceed.
+    console.log("Connected");
 });
 
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
-});
