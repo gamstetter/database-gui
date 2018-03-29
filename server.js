fc0c29c11fde323bb4aff08da2bfe313569adb04
Philip Tallo <tallopt@mail.uc.edu>
2018-03-29 14:38:55 -0700

    updated nodejs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 
+var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
@@ -9,14 +10,16 @@ const PORT = 8080;
 
 server.listen(PORT);
 
+app.use(express.static(__dirname + '/html'));
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/main.html');
+    res.sendFile(__dirname + '/html/main.html');
 });
 
 io.on('connection', function(socket){
     socket.on('process_query', function(queryString){
-        makeConnection();
-        let success = executeStatement(queryString);
+        let connection = makeConnection();
+        let success = executeStatement(queryString, connection);
         socket.emit('query_status', success);
     });
 });
@@ -40,9 +43,11 @@ function makeConnection(){
             console.log(JSON.stringify(err));
         }
     });
+
+    return connection;
 }
 
-function executeStatement(queryString) {
+function executeStatement(queryString, connection) {
     let request = new Request(queryString, function(err) {
         if (err) {
             return false;
